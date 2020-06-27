5b5a8d387139fc0852104db4611ea4a368423e1e
Felipe Fabossi <felipe.augusto.fabossi@ibm.com>
2020-06-27 17:19:37 -0300

    [FABOSSI] Css Changes

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-const express = require('express')
+const express = require('express');
 const gtts = require('gtts.js').gTTS
-const bodyparser = require('body-parser')
+const bodyparser = require('body-parser');
 
-const app = express()
+const app = express();
 
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({ extended: true }))
-app.set('view engine', 'ejs')
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index');
 })
 
 app.post('/', (req, res) => {
-    const text = req.body.text
-    const speech = new gtts(text)
-    speech.save("output.mp3")
-        .then(function () {
-            res.download("output.mp3")
-        }).catch(function (err) {
 
-        })
-})
+    const text = req.body.text;
+    const speech = new gtts(text);
+
+    speech
+        .save('output.mp3')
+        .then((data) => { res.download('output.mp3') })
+        .catch((err) => console.error(err));
+
+});
 
 app.use(express.static(__dirname + '/styles'));
 app.use(express.static(__dirname + '/images'));
 
 app.listen(5000, function () {
-    console.log("Server is listening on Port 5000")
-})
+    console.log('Server is listening on port 5000');
+});
+
